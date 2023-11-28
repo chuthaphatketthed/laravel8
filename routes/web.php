e4f8f4318b27028acbfe254453173769832860c7
chuthaphatketthed <150641578+chuthaphatketthed@users.noreply.github.com>
2023-11-28 20:37:11 +0700

    week3

--- routes/web.php
+++ routes/web.php
@@ -13,31 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::get("/teacher" , function (){
+	return view("teacher");
 });
 
-Route::get("/gallery", function () {
-    $ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
-    $bird = "https://images.indianexpress.com/2021/03/falcon-anthony-mackie-1200.jpg";
-    $cat = "http://www.onyxtruth.com/wp-content/uploads/2017/06/black-panther-movie-onyx-truth.jpg";
-    $god = "https://www.blackoutx.com/wp-content/uploads/2021/04/Thor.jpg";
-    $spider = "https://icdn5.digitaltrends.com/image/spiderman-far-from-home-poster-2-720x720.jpg";
-
-    return view("test/index", compact("ant", "bird", "cat", "god", "spider"));
+Route::get("/student" , function (){
+	return view("student");
 });
 
-Route::get("/gallery/ant", function () {
-    $ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
-    return view("test/ant", compact("ant"));
+Route::get("/theme" , function (){
+	return view("theme");
 });
 
-Route::get("/gallery/bird", function () {
-    $bird = "https://images.indianexpress.com/2021/03/falcon-anthony-mackie-1200.jpg";
-    return view("test/bird", compact("bird"));
+// Route Template Inheritance
+Route::get("/teacher/inheritance", function () {
+    return view("teacher-inheritance");
 });
-
-Route::get("/gallery/cat", function () {
-    $cat = "http://www.onyxtruth.com/wp-content/uploads/2017/06/black-panther-movie-onyx-truth.jpg";
-    return view("test/cat", compact("cat"));
+Route::get("/student/inheritance", function () {
+    return view("student-inheritance");
+});
+Route::get('/tables', function () {
+    return view('tables');
+});
+// Route Template Component
+Route::get("/teacher/component", function () {
+    return view("teacher-component");
+});
+Route::get("/student/component", function () {
+    return view("student-component");
 });
